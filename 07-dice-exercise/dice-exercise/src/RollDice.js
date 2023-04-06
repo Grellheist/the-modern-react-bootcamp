162ee18bc2625a77cb33d533837968164d18e111
grellheist <grellheist@tutanota.com>
2023-04-06 12:52:41 -0300

    Updated

--- 07-dice-exercise/dice-exercise/src/RollDice.js
+++ 07-dice-exercise/dice-exercise/src/RollDice.js
@@ -1,27 +1,36 @@
 import React, { Component } from 'react';
 import Die from './Die';
+import './RollDice.css';
 
 class RollDice extends Component {
     static defaultProps = {
         sides: ["one", "two", "three", "four", "five", "six"]
-    }
+    };
     constructor(props) {
         super(props);
-        this.state = {die1: "one", die2: "one"};
+        this.state = {die1: "one", die2: "one", rolling: false};
         this.handleClick = this.handleClick.bind(this);
     }
     handleClick () {
         this.setState({
             die1: this.props.sides[Math.floor(Math.random() * this.props.sides.length)],
-            die2: this.props.sides[Math.floor(Math.random() * this.props.sides.length)]
+            die2: this.props.sides[Math.floor(Math.random() * this.props.sides.length)],
+            rolling: true
         });
+        setTimeout(() => {
+            this.setState({rolling: false});
+        }, 1000);
     }
     render() {
         return (
-            <div>
-                <Die side={this.state.die1}/>
-                <Die side={this.state.die2}/>
-                <button onClick={this.handleClick}>Roll Dice</button>
+            <div className='RollDice'>
+                <div className='RollDice-container'>
+                    <Die side={this.state.die1} rolling={this.state.rolling}/>
+                    <Die side={this.state.die2} rolling={this.state.rolling}/>
+                </div>
+                <button onClick={this.handleClick} disabled={this.state.rolling}>
+                    {this.state.rolling ? <p>Rolling...</p> : <p>Roll Dice</p>}
+                </button>
             </div>
         );
     }
