028b0674df027affe93362ce8a2e4267daae0e90
grellheist <grellheist@tutanota.com>
2023-04-13 16:48:21 -0300

    Finished Lights out game

--- 12-lights-out/react-lights-out-starter/src/App.js
+++ 12-lights-out/react-lights-out-starter/src/App.js
@@ -2,16 +2,14 @@ import React, { Component } from "react";
 import Board from "./Board";
 import "./App.css";
 
-/** Simple app that just shows the LightsOut game. */
-
 class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <Board />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='App'>
+                <Board />
+            </div>
+        );
+    }
 }
 
 export default App;
