e7e459e7eb05e1a11ad3455acfaa42669e29442c
grellheist <grellheist@tutanota.com>
2023-04-15 14:09:45 -0300

    Finished Color Box maker exercise

---
+++ 14-forms-exercise/color-box-maker/src/NewBoxForm.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+class NewBoxForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { height: "", width: "", color: "" };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        const newBox = { ...this.state, id: uuidv4() }
+        this.props.createBox(newBox);
+        this.setState({ height: "", width: "", color: "" })
+    }
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <label>Height</label>
+                    <input
+                        type="text"
+                        name="height"
+                        value={this.state.height}
+                        onChange={this.handleChange}
+                        id="height"
+                    />
+                    <div>
+                        <label>Width</label>
+                        <input
+                            type="text"
+                            name="width"
+                            value={this.state.width}
+                            onChange={this.handleChange}
+                            id="width"
+                        />
+                    </div>
+                    <div>
+                        <label>Color</label>
+                        <input
+                            type="text"
+                            name="color"
+                            value={this.state.color}
+                            onChange={this.handleChange}
+                            id="color"
+                        />
+                    </div>
+                </div>
+                <button>Add new box</button>
+            </form>
+        );
+    }
+}
+
+export default NewBoxForm;
