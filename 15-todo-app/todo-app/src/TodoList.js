76acd16c649d29985487fe053f25c6fa02f0d2c9
grellheist <grellheist@tutanota.com>
2023-04-16 20:41:35 -0300

    Finished exercise

--- 15-todo-app/todo-app/src/TodoList.js
+++ 15-todo-app/todo-app/src/TodoList.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import Todo from "./Todo";
+import NewTodoForm from "./NewTodoForm";
 
 class TodoList extends Component {
     constructor(props) {
         super(props);
-        this.state = { todos: [{ task: "heya" }] };
+        this.state = { todos: [] };
         this.create = this.create.bind(this);
+        this.remove = this.remove.bind(this);
+        this.update = this.update.bind(this);
     }
     remove(id) {
         this.setState({
@@ -17,18 +20,29 @@ class TodoList extends Component {
             todos: [...this.state.todos, newTodo]
         })
     }
+    update(id, updatedTask) {
+        const updatedTodos = this.state.todos.map(todo => {
+            if (todo.id === id) {
+                return { ...todo, task: updatedTask };
+            }
+            return todo;
+        })
+        this.setState({ todos: updatedTodos })
+    }
     render() {
         const todos = this.state.todos.map(todo => (
             <Todo
                 key={todo.id}
                 id={todo.id}
-                removeTodo={() => this.remove(todo.id)}
+                removeTodo={this.remove}
                 task={todo.task}
+                updateTodo={this.update}
             />
         ));
         return (
             <div>
                 <h1>Todo list</h1>
+                <NewTodoForm createTodo={this.create} />
                 <ul>{todos}</ul>
             </div >
         )
