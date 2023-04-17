35ffb1a793ec7337d1bc556d5249278848859132
Grellheist <grellheist@tutanota.com>
2023-04-17 14:27:44 -0300

    Added starter files

--- 15-todo-app/todo-app/src/App.js
+++ 15-todo-app/todo-app/src/App.js
@@ -1,11 +1,15 @@
-import TodoList from './TodoList';
+import React, { Component } from "react";
+import TodoList from "./TodoList";
+import "./App.css";
 
-function App() {
+class App extends Component {
+  render() {
     return (
-        <div className="App">
-            <TodoList />
-        </div>
+      <div>
+        <TodoList />
+      </div>
     );
+  }
 }
 
 export default App;
